db42a538e7bc261c6770abe963ea89502f83a4c0
Obiwumma Pascal <pobiwumma@gmail.com>
2025-07-17 20:40:45 +0100

    Added files

---
+++ 10-react-quiz/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DateCounter from './DateCounter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div>
+    <DateCounter/>
+  </div>
+}
+
+export default App
