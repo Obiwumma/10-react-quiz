c60d2ca3ceb9d73c3201b87321702388da5c1a4c
Obiwumma Pascal <pobiwumma@gmail.com>
2025-07-18 20:36:58 +0100

    Added css code

--- 10-react-quiz/src/App.jsx
+++ 10-react-quiz/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import DateCounter from './DateCounter'
+import Header from './Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div>
-    <DateCounter/>
-  </div>
+  
+  return (
+  <div className="app">
+    <Header />
+  </div>)
 }
 
 export default App
